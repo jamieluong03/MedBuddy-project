0adab79f30604f70d0806cd848b23a098f25c0e2
Jamie Luong <jamieluong03@gmail.com>
2020-04-29 16:08:55 -0700

    added build

---
+++ client/build/precache-manifest.250ce6d9ee273a62bda5b8793b9ac9e2.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "167eefc485afc665da1bd47e616e78a0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc3933aabfc42862eccd",
+    "url": "/static/js/2.49bd1cb6.chunk.js"
+  },
+  {
+    "revision": "2ef422655e375d097023",
+    "url": "/static/js/main.168056c4.chunk.js"
+  },
+  {
+    "revision": "ff4797a82cc4cf4d6f31",
+    "url": "/static/js/runtime-main.4846cf47.js"
+  }
+]);
